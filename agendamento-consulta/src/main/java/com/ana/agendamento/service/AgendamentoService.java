49706f895125174764bfce31742ddb8cbe8c59c8
rosaletras <rosaletras@proton.me>
2026-03-26 16:07:21 -0300

    feat: implementacao da logica de negocio na camada service

--- agendamento-consulta/src/main/java/com/ana/agendamento/service/AgendamentoService.java
+++ agendamento-consulta/src/main/java/com/ana/agendamento/service/AgendamentoService.java
@@ -1,5 +1,21 @@
 package com.ana.agendamento.service;
 
+import com.ana.agendamento.model.Consulta;
+import com.ana.agendamento.repository.ConsultaRepository;
+import java.time.LocalDateTime;
+
 public class AgendamentoService {
-    
+    private ConsultaRepository repository;
+
+    public AgendamentoService(ConsultaRepository repository) {
+        this.repository = repository;
+    }
+
+    public boolean verificarDisponibilidade(LocalDateTime data) {
+        return true; 
+    }
+
+    public void agendar(Consulta consulta) {
+        repository.salvar(consulta);
+    }
 }
--- agendamento-consulta/src/main/java/com/ana/agendamento/service/AuthService.java
+++ agendamento-consulta/src/main/java/com/ana/agendamento/service/AuthService.java
@@ -1,5 +1,13 @@
 package com.ana.agendamento.service;
 
+import com.ana.agendamento.model.Usuario;
+
 public class AuthService {
-    
+    public boolean login(Usuario usuario, String login, String senha) {
+        return usuario.validarLogin(login, senha);
+    }
+
+    public void logout() {
+        System.out.println("[AUTH] Sessão encerrada.");
+    }
 }
